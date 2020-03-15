f17b54cae06557bfdfe7c23d9a19476444036b91
triest <you@example.com>
2020-03-16 01:02:16 +0300

    Go to twiter page

--- src/test/java/com/qna/habr/FirstTest.java
+++ src/test/java/com/qna/habr/FirstTest.java
@@ -8,14 +8,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
+import javax.lang.model.element.Element;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.*;
 
 public class FirstTest {
 
     @Test
-    public void FirstTest() {
+    public void FirstTest() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
         System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
@@ -35,39 +37,54 @@ public class FirstTest {
 
         //олучаем едеьенты
         List<WebElement> posts = driver.findElementsByClassName("content-list__item");
-
+        TimeUnit.SECONDS.sleep(2);
         System.out.println("posts");
         posts.get(2).click();
 
         //chech selenium tag exist
         //tags-list
-        /*
-        WebElement tagsList=  driver.findElementByXPath("//a[contains(text(),'Selenium')");
-        if(tagsList==null){
+
+        WebElement tagsList = driver.findElementByCssSelector(".question__tags:nth-child(2) > .tags-list a");
+        if (tagsList == null) {
             System.out.println("no found");
-        }else {
+        } else {
             System.out.println("found");
         }
-        */
+
 
         //open naw bar
 
+/*
         WebElement nawBar = driver.findElementByClassName("btn_navbar_toggle");
         if (nawBar == null) {
             System.out.println("nawBar no found");
             System.exit(1);
         } else {
             nawBar.click();
+            TimeUnit.SECONDS.sleep(2);
+            nawBar.click();
         }
+*/
+        driver.executeScript("window.scrollTo(0, document.body.scrollHeight*0.37);");
 
-        WebElement   sharing = driver.findElementByClassName("btn btn_share");
+        WebElement share = driver.findElementByCssSelector(".dropdown:nth-child(4) > .btn");
+        if (share != null) {
 
-        if (sharing == null) {
-            System.out.println("sharing no found");
-            System.exit(1);
+         share.click();
+            System.out.println("share not null");
         } else {
-            sharing.click();
+            System.out.println("share null");
         }
+
+
+        share = driver.findElementByCssSelector(".dropdown_active .link_tw");
+        if (share != null) {
+            share.click();
+            System.out.println("share not null");
+        } else {
+
+        }
+
     }
 
 
