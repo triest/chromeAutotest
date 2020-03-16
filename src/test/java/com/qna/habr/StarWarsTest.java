82f34688ff62188527ed78f0a0de7f6e1cdc242a
triest <you@example.com>
2020-03-17 00:45:51 +0300

    Add get ships list

--- src/test/java/com/qna/habr/StarWarsTest.java
+++ src/test/java/com/qna/habr/StarWarsTest.java
@@ -1,37 +1,112 @@
 package com.qna.habr;
 
-import com.github.cliftonlabs.json_simple.JsonException;
-
-import com.github.cliftonlabs.json_simple.Jsoner;
-import com.google.gson.*;
-
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
+
+import static org.apache.commons.lang3.ArrayUtils.add;
 
 public class StarWarsTest {
 
     @Test
     public void FirstTest() throws InterruptedException, ProtocolException {
+        System.out.println(FindHeroUrl());
+        String heroUrl = FindHeroUrl();
+        if (heroUrl == null) {
+            Assert.fail("Hero url Not Found");
+        }
+
+        ArrayList<String>  url= shackSteamshipArray(heroUrl);
+
+    }
+
+    public ArrayList<String> shackSteamshipArray(String Url) {
+
+        URL url = null;
+        try {
+            url = new URL(Url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection con = null;
+
+        try {
+            con = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            con.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
+        con.setRequestProperty("Accept", "application/json");
+        int status = 0;
+        try {
+            status = con.getResponseCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String inputLine = null;
+        StringBuffer content = new StringBuffer();
+        while (true) {
+            try {
+                if (!((inputLine = in.readLine()) != null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            content.append(inputLine);
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String responseString = content.toString();
+        JSONObject jsonObject = new JSONObject(responseString);
+        JSONArray arr = jsonObject.getJSONArray("starships");
+        System.out.println(arr);
+        String[] name = new String[]{};
+             ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < arr.length(); i++) {
+            //list.add(arr2.getJSONObject(i).getString("name"));
+            System.out.println(arr.getJSONObject(i).getString("name"));
+            list.add(arr.getJSONObject(i).getString("name"));
+        }
+
+        return list;
+
+    }
+
+
+    public String FindHeroUrl() throws InterruptedException, ProtocolException {
 
         //  String query = "access_token=" + URLEncoder.encode(AUTH_TOKEN, CHARSET);
 
         Boolean findPadmé = false;
 
-        String Address="https://swapi.co/api/people/";
-
-        while (findPadmé==false) {
-
+        String Address = "https://swapi.co/api/people/";
+        int count=0;
+        while (findPadmé == false || count<20) {
+            count++;
             URL url = null;
             try {
                 url = new URL(Address);
@@ -85,26 +160,28 @@ public class StarWarsTest {
             String responseString = content.toString();
             JSONObject jsonObject = new JSONObject(responseString);
             JSONArray arr = jsonObject.getJSONArray("results");
+            int Key = 0;
             for (int i = 0; i < arr.length(); i++) {
                 String name = arr.getJSONObject(i).getString("name");
                 if (name.equals("Padmé Amidala")) {
                     findPadmé = true;
+                    Key = i;
                 }
             }
 
             if (findPadmé == false) {
-                System.out.println("Padme not found");
-                System.out.println(jsonObject.get("next").toString());
                 Address = jsonObject.get("next").toString();
 
-                if(jsonObject.get("next").toString()==null){
+                if (jsonObject.get("next").toString() == null) {
                     break;
                 }
-            }else {
+            } else {
                 System.out.println("Padme found");
-                break;
+                String urlHero = arr.getJSONObject(Key).getString("url");
+                return urlHero;
             }
         }
+        return null;
     }
 
 
