7a2f43b20d14c7cf370f3fda75a20b98ac1dfd12
triest <you@example.com>
2020-03-19 00:23:56 +0300

    Add exit from driver

--- src/test/java/com/qna/habr/FirstTest.java
+++ src/test/java/com/qna/habr/FirstTest.java
@@ -16,6 +16,73 @@ import java.util.concurrent.TimeUnit;
 public class FirstTest {
 
     @Test
+    public void PleseholderTest() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().setSize(new Dimension(1080, 960));
+        driver.get("https://qna.habr.com/");
+        try {
+            Thread.sleep(1000);  // Let the user actually see something!
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*
+         * check plaseholder
+         * */
+        String s = driver.findElement(By.xpath("//input[@placeholder='Найти вопрос, ответ, тег или пользователя']")).getAttribute("placeholder");
+        driver.quit();
+        if (s.equals("Найти вопрос, ответ, тег или пользователя")) {
+        } else {
+            Assert.fail("placeholder not found");
+        }
+    }
+
+    @Test
+    public void CheckSeleniumTeg() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().setSize(new Dimension(1080, 960));
+        driver.get("https://qna.habr.com/");
+        try {
+            Thread.sleep(1000);  // Let the user actually see something!
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium");
+        searchBox.sendKeys(Keys.UP);
+        searchBox.sendKeys(Keys.UP);
+        searchBox.sendKeys(Keys.ENTER);
+
+        //олучаем едеьенты
+        List<WebElement> posts = driver.findElementsByClassName("content-list__item");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        posts.get(2).click();
+
+        WebElement tagsList = driver.findElementByXPath("//a[contains(text(),'Selenium')]");
+        driver.quit();
+        if (tagsList == null) {
+            Assert.fail("tags not found");
+        } else {
+            Assert.assertTrue(true);
+        }
+    }
+
+
+
+
+    @Test
     public void FirstTest() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
@@ -30,11 +97,11 @@ public class FirstTest {
         }
 
         /*
-        * check plaseholder
-        * */
-        String s=driver.findElement(By.xpath("//input[@placeholder='Найти вопрос, ответ, тег или пользователя']")).getAttribute("placeholder");
-        if(s.equals("Найти вопрос, ответ, тег или пользователя")){
-        }else {
+         * check plaseholder
+         * */
+        String s = driver.findElement(By.xpath("//input[@placeholder='Найти вопрос, ответ, тег или пользователя']")).getAttribute("placeholder");
+        if (s.equals("Найти вопрос, ответ, тег или пользователя")) {
+        } else {
             Assert.fail("placeholder not found");
         }
 
@@ -53,6 +120,7 @@ public class FirstTest {
         WebElement tagsList = driver.findElementByXPath("//a[contains(text(),'Selenium')]");
 
         if (tagsList == null) {
+            driver.quit();
             Assert.fail("tags not found");
         }
 
@@ -62,6 +130,7 @@ public class FirstTest {
 
         WebElement nawBar = driver.findElementByClassName("btn_navbar_toggle");
         if (nawBar == null) {
+            driver.quit();
             Assert.fail("nawbar not found");
             System.exit(1);
         } else {
@@ -74,8 +143,9 @@ public class FirstTest {
 
         WebElement share = driver.findElementByCssSelector(".dropdown:nth-child(4) > .btn");
         if (share != null) {
-         share.click();
+            share.click();
         } else {
+            driver.quit();
             Assert.fail("share not found");
         }
         TimeUnit.SECONDS.sleep(2);
@@ -88,16 +158,16 @@ public class FirstTest {
         }
         TimeUnit.SECONDS.sleep(2);
         // check Tritter Page on new window
-        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
-        System.out.println(driver.getCurrentUrl().toString());
-        String new_url=driver.getCurrentUrl().toString();
-        if(new_url.contains("/twitter.com/intent/tweet?text=")){
-            System.out.println("pass");
-        }else {
+        String new_url = driver.getCurrentUrl().toString();
+        driver.quit();
+        if (new_url.contains("/twitter.com/intent/tweet?text=")) {
+            Assert.assertTrue(true);
+        } else {
+
             Assert.fail("Twittrer page not open");
         }
-        driver.quit();
     }
 
 
